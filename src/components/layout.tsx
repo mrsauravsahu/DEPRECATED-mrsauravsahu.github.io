6ff92f37220d71e4b57872cf4c063da75085b71c
Saurav Sahu <sauravsahu.12@live.com>
2019-10-30 00:20:10 +0530

    create a main page and add styling (#28)

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,43 +7,17 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
-import './layout.css'
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '0px 1.0875rem 1.45rem',
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          Made with&nbsp;
-          <span role="img" aria-label="heart emoji">❤️</span>
-          &nbsp;by Saurav
-        </footer>
-      </div>
-    </>
-  )
-}
+const Layout = ({ children }) => (
+  <>
+    <main>{children}</main>
+    <footer>
+      Made with&nbsp;
+      <span role="img" aria-label="heart emoji">❤️</span>
+      &nbsp;by Saurav
+    </footer>
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
