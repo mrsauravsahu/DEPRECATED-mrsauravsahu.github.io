b8fdd7bcaa4fb903f5ea972d3b5e775333cd8042
Saurav Sahu <sauravsahu.12@live.com>
2019-10-29 23:05:37 +0530

    clean up main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 import { Projects } from '../containers/projects'
 import { About } from '../containers/about'
@@ -10,13 +7,7 @@ import { About } from '../containers/about'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Portfolio</h1>
-    <p>Welcome to Saurav&apos;s portfolio.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h2>Welcome to Saurav&apos;s portfolio.</h2>
 
     <Projects />
     <About />
