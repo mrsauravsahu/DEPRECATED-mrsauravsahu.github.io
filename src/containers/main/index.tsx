63f30462dba12a25e12184cd246e51576587d735
Saurav Sahu <sauravsahu.12@live.com>
2019-10-30 22:12:42 +0530

    style main page

--- src/containers/main/index.tsx
+++ src/containers/main/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import classnames from 'classnames'
 
 import main from '../../data/main.json'
 import styles from './index.module.css'
@@ -6,5 +7,6 @@ import styles from './index.module.css'
 export const Main = () => (
   <div className={styles.container}>
     <h1 className={styles.command}>{`./${main.name}`}</h1>
+    <h1 className={classnames(styles.command, styles.option)}>--ls</h1>
   </div>
 )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import Layout from '../components/layout'
+import { FullPage, Slide } from 'react-full-page'
+
 import SEO from '../components/seo'
 import { Projects } from '../containers/projects'
 import { About } from '../containers/about'
 import { Main } from '../containers/main'
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
-
-    <Main />
-    <Projects />
-    <About />
-  </Layout>
+    <FullPage>
+      <Slide>
+        <Main />
+      </Slide>
+      <Slide>
+        <Projects />
+      </Slide>
+      <Slide>
+        <About />
+      </Slide>
+    </FullPage>
+  </>
 )
 
 export default IndexPage
