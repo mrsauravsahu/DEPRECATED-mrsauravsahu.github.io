d6d5fc13b69a657134042b3b9ef155a0c56b6916
Saurav Sahu <sauravsahu.12@live.com>
2019-10-31 01:33:49 +0530

    styling: add basic styling to all the pages (#30)
    * install react-full-page package
    
    * install classnames package
    
    * style main page
    
    * install react-pose
    
    * install react-pose-text
    
    * add all sections to main json
    
    * complete animation for listing sections in main page
    
    * update projects page and add title styling
    
    * add basic styling for projects page
    
    * add basic styles for about page
    
    * add ability to pass className prop to tile component
    
    * update project command option name
    
    * add bottom margin for tile component's title
    
    * increate page sub header font size
    
    * remove useless selector from projects page css
    
    * make zeroth pagedefault

--- src/components/tile/index.tsx
+++ src/components/tile/index.tsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import classnames from 'classnames'
+
+import styles from './tile.module.css'
 
 export type TileProps = {
-    title: string,
-    description: string
+  title: string,
+  description: string,
+  renderSpine?: boolean,
+  renderHero?: boolean,
+  className?: {
+    container?: string
+  }
 }
 
-export const Tile: React.FC<TileProps> = ({ title, description }) => (
-  <div>
-    <div>{title}</div>
-    <div>{description}</div>
+export const Tile: React.FC<TileProps> = ({
+  title,
+  description,
+  renderSpine = true,
+  renderHero = true,
+  className = {
+    container: '',
+  },
+}) => (
+  <div className={classnames(styles.rootContainer, className.container)}>
+    {renderSpine ? <div className={styles.tileSpine} /> : null}
+    <div className={styles.contentContainer}>
+      {renderHero ? <div className={styles.hero}>{title[0]}</div> : null}
+      <div className={styles.title}>{title}</div>
+      <div>{description}</div>
+    </div>
   </div>
 )
