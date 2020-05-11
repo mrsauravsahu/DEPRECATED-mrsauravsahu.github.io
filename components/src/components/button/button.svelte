2cda77be5d268b003ffaa693469ef49c1acf59f9
Saurav Sahu <sauravsahu.12@live.com>
2020-05-11 13:05:42 +0530

    update button styling and global variable names

--- components/src/components/button/button.svelte
+++ components/src/components/button/button.svelte
@@ -1,30 +1,36 @@
-<svelte:options tag="ss-button" />
-
-<button class="button" on:click={onClick}>
-    {text}
-</button>
-
-<style>
-    .button {
-      border: 1px solid #eee;
-      border-radius: 3px;
-      background-color: #FFFFFF;
-      cursor: pointer;
-      font-size: 15px;
-      padding: 3px 10px;
-      margin: 10px;
-      font-weight: 900;
-    }
-</style>
-
 <script>
-  import { createEventDispatcher } from 'svelte';
-  
-  export let text = '';
+  import { createEventDispatcher } from "svelte";
+
+  export let text = "";
 
   const dispatch = createEventDispatcher();
 
   function onClick(event) {
-    dispatch('click', event)
+    dispatch("click", event);
   }
 </script>
+
+<style>
+  button {
+    outline: none;
+    border: var(--ss-border-width) solid rgb(var(--ss-bg));
+    cursor: pointer;
+    font-size: 1rem;
+    padding: 1rem;
+    font-weight: 800;
+    border-radius: 2rem;
+
+    color: rgb(var(--ss-bg));
+    background-color: transparent;
+  }
+
+  button:focus,button:active{
+    color: rgb(var(--ss-accent));   
+    border: var(--ss-border-width) solid rgba(var(--ss-bg), 0.5);
+    background-color: rgb(var(--ss-bg));
+  }
+</style>
+
+<svelte:options tag="ss-button" />
+
+<button on:click={onClick}>{text}</button>
