6da350465857b9b5d9af2c579c7b542b9b95d540
Saurav Sahu <sauravsahu.12@live.com>
2020-05-16 01:00:56 +0530

    site: add basic info in index page

--- site/src/routes/index.svelte
+++ site/src/routes/index.svelte
@@ -20,38 +20,81 @@
   .about-me {
     margin: 0 4rem;
   }
+
+  .projects-container > div {
+    margin-bottom: 1rem;
+  }
 </style>
 
 <svelte:head>
   <title>Sapper project template</title>
 </svelte:head>
 
-<ss-title>--saurav-sahu</ss-title>
-<div class="tech-container">
-  <ss-bubble text="react" />
-  <ss-bubble text="svelte" />
-  <ss-bubble text=".net core" />
-  <ss-bubble text="docker" />
-  <ss-bubble text="typescript" />
-  <ss-bubble text="azure" />
-  <ss-bubble text="node" />
-  <ss-bubble text="react native" />
+<div class="main-container">
+  <ss-title>--saurav-sahu</ss-title>
+  <div class="tech-container">
+    <ss-bubble text=".net core" />
+    <ss-bubble text="svelte" />
+    <ss-bubble text="typescript" />
+    <ss-bubble text="docker" />
+    <ss-bubble text="node" />
+    <ss-bubble text="azure" />
+    <ss-bubble text="react native" />
+    <ss-bubble text="react" />
+  </div>
+  <div class="about-container">
+    <div class="about-photo-container">
+      <img src="me.jpg" alt="Saurav Sahu" />
+    </div>
+    <div class="about-me">
+      <p>Digital consultant and full stack engineer at Digital McKinsey.</p>
+      <p>
+        Fascinated about how things work, learning algorithms and how they’re
+        used to solve real world problems. I create web and mobile applications
+        ready for use in our every-day lives.
+      </p>
+      <p>
+        On the server side of things, I use ASP .NET Core and Node to create
+        APIs. On the browser, it's mainly React to create web applications;
+        React Native and Xamarin.Forms to create mobile applications.
+      </p>
+      <p>
+        Learning is a constant here, and currently, I’m learning Svelte to
+        increase my frontend skills and delving into the Cloud with Microsoft
+        Azure.
+      </p>
+    </div>
+  </div>
 </div>
 
-<div class="about-container">
-  <div class="about-photo-container">
-    <img src="me.jpg" alt="Saurav Sahu" />
+<div class="projects-container">
+  <ss-title>--projects</ss-title>
+  <div>
+    <ss-card title="git-remote-to-url">
+      Converts git's remote url to a valid url to open in the browser.
+    </ss-card>
   </div>
-  <div class="about-me">
-    <p>
-      Digital consultant and full stack engineer at Digital McKinsey. Fascinated
-      about how things work. I really like learning algorithms and how they’re
-      used to solve real world problems. I create web and mobile applications
-      ready for use in our every-day lives. On the client side of things, I use
-      React to create web applications; React Native and Xamarin.Forms to create
-      mobile applications. On the server, I use ASP .NET Core and Node to create
-      APIs. Learning is a constant here, and currently, I’m learning Svelte to
-      increase my frontend skills and delving into Cloud with Microsoft Azure.
-    </p>
+  <div>
+    <ss-card title="TowerOfHanoi">
+      <p>
+        Cross platform Tower of Hanoi game using GLUT for Windows and freeglut
+        for Linux and Mac.
+      </p>
+    </ss-card>
   </div>
+  <div>
+    <ss-card title="mrsauravsahu.github.io">
+      <p>
+        A little site that tells the world who I am.
+      </p>
+    </ss-card>
+  </div>
+</div>
+
+<div class="social-container">
+  <ss-title>--socials</ss-title>
+  <ss-button text="github" />
+  <ss-button text="twitter" />
+  <ss-button text="unsplash" />
+  <ss-button text="instagram" />
 </div>
