a587fe23cdd613cb57f4739df7e036ace0eb6e24
Saurav Sahu <sauravsahu.12@live.com>
2020-05-17 02:00:42 +0530

    site: update links on socials

--- site/src/pages/home/socials.svelte
+++ site/src/pages/home/socials.svelte
@@ -1,7 +1,19 @@
+<script>
+  import { goto } from "@sapper/app";
+</script>
+
 <div class="social-container">
   <ss-title>--socials</ss-title>
-  <ss-button text="github" />
-  <ss-button text="twitter" />
-  <ss-button text="unsplash" />
-  <ss-button text="instagram" />
+  <ss-button
+    text="github"
+    on:click={() => goto('https://github.com/mrsauravsahu')} />
+  <ss-button
+    text="twitter"
+    on:click={() => goto('https://twitter.com/mrsauravsahu')} />
+  <ss-button
+    text="unsplash"
+    on:click={() => goto('https://unsplash.com/@mrsauravsahu')} />
+  <ss-button
+    text="instagram"
+    on:click={() => goto('https://instagram.com/mrsauravsahu')} />
 </div>
