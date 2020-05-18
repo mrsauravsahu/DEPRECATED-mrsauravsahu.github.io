cf15638e42a238ff7e713e3d4026129683758e0d
Saurav Sahu <sauravsahu.12@live.com>
2020-05-18 18:21:55 +0530

    site: feat - add basic theme support

--- site/src/routes/index.svelte
+++ site/src/routes/index.svelte
@@ -1,12 +1,37 @@
 <script>
+  import { onDestroy, onMount } from "svelte";
+
   import About from "../pages/home/about.svelte";
   import Projects from "../pages/home/projects.svelte";
   import Socials from "../pages/home/socials.svelte";
+
+  import { theme } from "../stores/theme";
+  import { setThemeVariables, THEMES } from "../utils/theme";
+
+  let currentTheme, themeSubscription;
+
+  onMount(() => {
+    themeSubscription = theme.subscribe(theme => {
+      currentTheme = theme;
+
+      // Update CSS variables
+      setThemeVariables(theme);
+    });
+    
+    onDestroy(themeSubscription);
+  });
+
+  const toggleTheme = () => {
+    const nextTheme = currentTheme == THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
+    theme.set(nextTheme);
+  };
 </script>
 
 <svelte:head>
   <title>Saurav Sahu</title>
 </svelte:head>
+
+<ss-button on:click={toggleTheme} text="toggle theme" />
 
 <About />
 <Projects />
---
+++ site/src/utils/theme.js
@@ -0,0 +1,37 @@
+export const THEMES = {
+    LIGHT: 'light',
+    DARK: 'dark'
+};
+
+const setThemeFromResourceDictionary = (theme, element) => {
+    const resourceDictionary = {
+        [THEMES.LIGHT]: {
+            '--ss-accent': '33, 42, 61',
+            '--ss-bg': '243, 248, 248',
+            '--site-image-filter': 'none',
+        },
+        [THEMES.DARK]: {
+            '--ss-accent': '160, 160, 160',
+            '--ss-bg': '30, 30, 30',
+            '--ss-border-width': '0.25rem',
+            '--ss-border-radius': '1rem',
+            '--ss-base-font-size': '1rem',
+            '--site-image-filter': 'grayscale()',
+        }
+    }
+
+    var themeData = resourceDictionary[theme];
+
+    Object
+        .keys(themeData)
+        .forEach(key => {
+            element.style.setProperty(key, themeData[key]);
+        })
+}
+
+export const setThemeVariables = (theme) => {
+    if (theme) {
+        const rootElement = document.documentElement;
+        setThemeFromResourceDictionary(theme, rootElement);
+    }
+}
