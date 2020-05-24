952ce1e490163e396dbac214b4c5931f613306c9
Saurav Sahu <sauravsahu.12@live.com>
2020-05-24 14:36:17 +0530

    site: fix: styling for blog index page

--- site/src/routes/blog/index.svelte
+++ site/src/routes/blog/index.svelte
@@ -15,6 +15,11 @@
 </script>
 
 <style>
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+
   ul {
     padding: 0;
     margin: 0;
@@ -34,16 +39,21 @@
   <title>Blog</title>
 </svelte:head>
 
-<ul>
-  {#each posts as post}
-    <!-- we're using the non-standard `rel=prefetch` attribute to
+<div class="container">
+  <div class="content">
+    <p>Here a few thoughts that I thought should be written down...</p>
+    <ul>
+      {#each posts as post}
+        <!-- we're using the non-standard `rel=prefetch` attribute to
 				tell Sapper to load the data for the page as soon as
 				the user hovers over the link or taps it, instead of
 				waiting for the 'click' event -->
-    <li>
-      <a rel="prefetch" href="blog/{blogSlugGenerator(post)}">
-        <ss-card title={post.title}>{post.line}</ss-card>
-      </a>
-    </li>
-  {/each}
-</ul>
+        <li>
+          <a rel="prefetch" href="blog/{blogSlugGenerator(post)}">
+            <ss-card title={post.title}>{post.line}</ss-card>
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </div>
+</div>
