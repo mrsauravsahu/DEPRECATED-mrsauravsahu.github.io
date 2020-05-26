811ed482aa792eaae411467503d6e7b1c2dd6eb1
mrsauravsahu <mrsauravsahu@outlook.com>
2020-05-26 17:16:43 +0530

    site: add email for contact

--- site/src/pages/home/socials.svelte
+++ site/src/pages/home/socials.svelte
@@ -4,6 +4,10 @@
 
 <ss-title id="socials">--socials</ss-title>
 <div class="social-container content">
+  <p>Here's how you can get in touch with me 😊️</p>
+  <ss-button
+    text="mrsauravsahu@outlook.com"
+    on:click={() => goto('mailto:mrsauravsahu@outlook.com')} />
   <ss-button
     text="github"
     on:click={() => goto('https://github.com/mrsauravsahu')} />
