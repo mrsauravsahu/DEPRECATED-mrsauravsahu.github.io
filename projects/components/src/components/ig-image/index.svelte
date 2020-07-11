3b232fa783d6b22bea74ff63b04b30b7520c6a7d
Saurav Sahu <sauravsahu.12@live.com>
2020-07-11 13:22:23 +0530

    load image with JS to fix empty borders

--- projects/components/src/components/ig-image/index.svelte
+++ projects/components/src/components/ig-image/index.svelte
@@ -1,22 +1,46 @@
 <script>
+  import { onMount } from "svelte";
+
   export let src;
   export let message;
 
   let isClicked = false;
+  let isImageLoaded = false;
 
   const onImageClicked = () => {
     isClicked = true;
   };
+
+  onMount(() => {
+    setTimeout(() => {
+      const imageLoader = new Image();
+      imageLoader.onload = () => {
+        isImageLoaded = true;
+      };
+      imageLoader.src = src;
+    }, 0);
+  });
 </script>
 
 <style>
+  .root {
+    display: flex;
+    flex-direction: column;
+  }
+
   .message {
     position: relative;
-    width: 100%;
     height: 100%;
     text-align: center;
     opacity: 0;
     transition: 0.2s linear;
+    align-self: center;
+    background-color: rgb(var(--ss-accent));
+    color: rgb(var(--ss-bg));
+    margin: 0.5rem;
+    padding: 0.25rem 2rem;
+    border-radius: 1rem;
+    font-size: calc(0.75 * var(--ss-base-font-size));
   }
 
   .message--image-clicked {
@@ -42,13 +66,15 @@
 </style>
 
 <svelte:options tag="ss-ig-image" />
-<div>
-  <!-- 
+{#if isImageLoaded}
+  <div class="root">
+    <!-- 
     TODO: Research how to fix alt prop in svelte 
     error: cannot read property match of null
   -->
-  <div class="image__container">
-    <img {src} alt="instagram style" on:click={onImageClicked} />
+    <div class="image__container">
+      <img {src} alt="instagram style" on:click={onImageClicked} />
+    </div>
+    <div class:message class:message--image-clicked={isClicked}>{message}</div>
   </div>
-  <div class:message class:message--image-clicked={isClicked}>{message}</div>
-</div>
+{/if}
