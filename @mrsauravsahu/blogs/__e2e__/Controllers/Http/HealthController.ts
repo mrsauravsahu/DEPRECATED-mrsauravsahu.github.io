7c4265301191a066785b5b6087a64396dc710af6
Saurav Sahu <sauravsahu.12@live.com>
2020-07-12 00:36:39 +0530

    format fix

--- @mrsauravsahu/blogs/__e2e__/Controllers/Http/HealthController.ts
+++ @mrsauravsahu/blogs/__e2e__/Controllers/Http/HealthController.ts
@@ -1,10 +1,9 @@
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
-import supertest from 'supertest'
+import supertest from 'supertest';
 
 test('getHealth', async () => {
-  const result = await supertest(process.env.BASE_URL)
-    .get('/');
+  const result = await supertest(process.env.BASE_URL).get('/')
 
   assert.is(result.status, 200);
   assert.equal(result.body, {
