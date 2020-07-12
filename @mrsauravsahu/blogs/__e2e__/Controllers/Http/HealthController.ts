4d261e4e7d1955bd0b43f0457fc5b906fb6b7cbc
Saurav Sahu <sauravsahu.12@live.com>
2020-07-12 14:02:21 +0530

    switch to jest  for testing

--- @mrsauravsahu/blogs/__e2e__/Controllers/Http/HealthController.ts
+++ @mrsauravsahu/blogs/__e2e__/Controllers/Http/HealthController.ts
@@ -1,17 +1,13 @@
-import { test } from 'uvu';
-import * as assert from 'uvu/assert';
 import supertest from 'supertest';
 
-test('getHealth', async () => {
+it('getHealth', async () => {
   const result = await supertest(process.env.BASE_URL).get('/')
 
-  assert.is(result.status, 200);
-  assert.equal(result.body, {
+  expect(result.status).toBe(200);
+  expect(result.body).toEqual({
     data: {
       name: 'blogs',
       version: 'v0.0.0'
     }
   });
 })
-
-test.run()
