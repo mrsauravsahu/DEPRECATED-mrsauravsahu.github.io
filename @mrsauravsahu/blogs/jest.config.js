e4a16135bb0337053f3d22b3ccfa1a6f36cd5aae
Saurav Sahu <sauravsahu.12@live.com>
2020-07-14 10:28:44 +0530

    fix: typeScript paths for jest

--- @mrsauravsahu/blogs/jest.config.js
+++ @mrsauravsahu/blogs/jest.config.js
@@ -1,3 +1,6 @@
+const tsconfig = require("./tsconfig.test.json")
+const moduleNameMapper = require("tsconfig-paths-jest")(tsconfig)
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
@@ -7,10 +10,12 @@ module.exports = {
   },
   globals: {
     'ts-jest': {
-      packageJson: 'package.json'
+      packageJson: 'package.json',
+      tsConfig: '<rootDir>/tsconfig.test.json'
     }
   },
   setupFiles: [
     "<rootDir>/jest.setup.ts"
-  ]
+  ],
+  moduleNameMapper
 };
