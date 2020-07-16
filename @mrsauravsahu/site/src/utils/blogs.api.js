2457413704ee8ae747490512c8ad64d5c9af4cba
Saurav Sahu <sauravsahu.12@live.com>
2020-07-16 16:57:39 +0530

    update blogs api util and add download post file api call

--- @mrsauravsahu/site/src/utils/blogs.api.js
+++ @mrsauravsahu/site/src/utils/blogs.api.js
@@ -1,8 +1,27 @@
+import { Constants } from "../constants";
+
 export const getBlogsAsync = async (fetch) => {
-    const dataUrl = `${process.env.BLOGS_BASE_URL}/blogs`;
+    const dataUrl = `${Constants.BLOGS_BASE_URL}/blogs`;
     console.log(`Fetching blogs from: ${dataUrl}`)
     const allBlogsResponse = await fetch(dataUrl)
     const json = await allBlogsResponse.json()
     const { data: blogs } = json;
     return blogs;
+}
+
+export const getBlogByIdAsync = async (fetch, blogId) => {
+    const dataUrl = `${Constants.BLOGS_BASE_URL}/blogs/${blogId}`;
+    console.log(`Fetching blog with id: ${blogId} from ${dataUrl}`);
+    const blogByIdResponse = await fetch(dataUrl)
+    const json = await blogByIdResponse.json()
+    const { data: blog } = json;
+    return blog;
+}
+
+export const getBlogPostContentAsync = async (fetch, { blogId, postId }) => {
+    const dataUrl = `${Constants.BLOGS_BASE_URL}/blogs/${blogId}/posts/${postId}/file`;
+    console.log(`Fetching blog post file with blogId: ${blogId} and postId: ${postId} from ${dataUrl}`);
+    const fileResponse = await fetch(dataUrl)
+    const fileText = await fileResponse.text()
+    return fileText
 }
