3401e265bfbf3ff33804be37a6d0b7766671d635
Saurav Sahu <sauravsahu.12@live.com>
2020-07-22 02:08:42 +0530

    supress errors and fix input properties

--- @mrsauravsahu/site/rollup.config.js
+++ @mrsauravsahu/site/rollup.config.js
@@ -14,11 +14,17 @@ const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
-const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
+const onwarn = (warning, onwarn) => {
+	if (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) return;
+	if (warning.code === 'THIS_IS_UNDEFINED') return;
+	if (warning.code === 'EMPTY_BUNDLE') return;
+	if (warning.code === 'PLUGIN_WARNING') return;
+	onwarn(warning);
+}
 
 export default {
 	client: {
-		input: config.client.input(),
+		input: config.client.input().replace(/\.js$/, '.ts'),
 		output: config.client.output(),
 		plugins: [
 			json(),
@@ -66,7 +72,7 @@ export default {
 	},
 
 	server: {
-		input: config.server.input(),
+		input: config.server.input().server.replace(/\.js$/, '.ts'),
 		output: config.server.output(),
 		plugins: [
 			json(),
@@ -93,7 +99,7 @@ export default {
 	},
 
 	serviceworker: {
-		input: config.serviceworker.input(),
+		input: config.serviceworker.input().replace(/\.js$/, '.ts'),
 		output: config.serviceworker.output(),
 		plugins: [
 			resolve(),
