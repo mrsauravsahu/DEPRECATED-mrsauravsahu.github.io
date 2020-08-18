d9ff660bdda57966d950edaacff32171d69efad9
Saurav Sahu <mrsauravsahu@outlook.com>
2020-08-18 20:46:52 +0530

    WIP: replace adonis API with .NET Core Web API

---
+++ @mrsauravsahu/blogs/Controllers/AppController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace blogs.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public dynamic GetAppInfo()
+        {
+            return new {
+                Name = "blogs",
+                Version = "0.0.0-alpha.1"
+            };
+        }
+    }
+}
