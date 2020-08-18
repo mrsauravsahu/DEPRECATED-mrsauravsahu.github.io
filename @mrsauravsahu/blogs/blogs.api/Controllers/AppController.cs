223c19319e2972c685a175e3567691f248469561
Saurav Sahu <mrsauravsahu@outlook.com>
2020-08-18 22:44:09 +0530

    use LocalFileService in api

--- @mrsauravsahu/blogs/blogs.api/Controllers/AppController.cs
+++ @mrsauravsahu/blogs/blogs.api/Controllers/AppController.cs
@@ -1,5 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 using blogs.api.Contracts;
+using blogs.services;
 using Microsoft.AspNetCore.Mvc;
 using Propfull.AspNet.Config;
 
@@ -9,15 +14,26 @@ namespace blogs.api.Controllers
     [Route("")]
     public class AppController : ControllerBase
     {
-public AppController(ConfigService<AppAboutConfig> configService){
-    this.configService = configService;
-}
-private readonly ConfigService<AppAboutConfig> configService ;
+        public AppController(ConfigService<AppAboutConfig> configService,
+        LocalFileService localFileService)
+        {
+            this.configService = configService;
+            this.localFileService = localFileService;
+        }
+        private readonly ConfigService<AppAboutConfig> configService;
+        private readonly LocalFileService localFileService;
 
         [HttpGet]
         public async Task<AppAboutConfig> GetAppInfo()
         {
             var aboutConfig = await configService.GetConfigAsync();
+
+            var fileName = $"{DateTime.UtcNow.ToString().Replace("/", "-")}.txt";
+            var data = JsonSerializer.Serialize(aboutConfig);
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(data));
+            
+            await localFileService.SaveBlobAsync("dotnet", fileName, stream);
+
             return aboutConfig;
         }
     }
