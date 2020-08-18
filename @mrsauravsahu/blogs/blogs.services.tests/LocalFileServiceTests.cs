422e08318390d146988723aa7ce48971708920d9
Saurav Sahu <mrsauravsahu@outlook.com>
2020-08-18 22:22:27 +0530

    implement SaveBlobAsync

--- @mrsauravsahu/blogs/blogs.services.tests/LocalFileServiceTests.cs
+++ @mrsauravsahu/blogs/blogs.services.tests/LocalFileServiceTests.cs
@@ -2,22 +2,26 @@ using System;
 using Xunit;
 using blogs.services.contracts;
 using blogs.services;
+using System.IO;
 
 namespace blogs.services.tests
 {
     public class LocalFileServiceTests
     {
-private readonly IBlobService sut;
+        private string sampleBasePath = nameof(sampleBasePath);
+        private readonly IBlobService sut;
 
         public LocalFileServiceTests()
         {
-sut = new LocalFileService();
+            sut = new LocalFileService(sampleBasePath);
         }
 
         [Fact]
-        public void Test1()
+        public void ShouldSaveAFileToSpecifiedContainer()
         {
-
+            // Create fake objects for testing
+            var sampleFile = new MemoryStream();
+            sut.SaveBlobAsync("container", "file.md", sampleFile);
         }
     }
 }
