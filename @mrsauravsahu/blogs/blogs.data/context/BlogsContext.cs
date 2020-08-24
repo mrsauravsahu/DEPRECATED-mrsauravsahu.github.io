1f4e2c38c97b332606a77c86dd0795cf519c0d24
Saurav Sahu <mrsauravsahu@outlook.com>
2020-08-25 01:13:52 +0530

    create a blog table

--- @mrsauravsahu/blogs/blogs.data/context/BlogsContext.cs
+++ @mrsauravsahu/blogs/blogs.data/context/BlogsContext.cs
@@ -8,5 +8,15 @@ namespace blogs.data.context
         public BlogsContext(DbContextOptions<BlogsContext> options) : base(options) { }
 
         public DbSet<Link> Links { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Blog>()
+                .Property<string>("images")
+                .HasField("images")
+                .HasColumnName("Images");
+        }
     }
 }
