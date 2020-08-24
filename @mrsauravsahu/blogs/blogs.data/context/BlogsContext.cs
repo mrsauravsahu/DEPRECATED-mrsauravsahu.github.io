26278457e58722569ad0173d861f5a083ad2e473
Saurav Sahu <mrsauravsahu@outlook.com>
2020-08-24 23:57:36 +0530

    try connecting to db

--- @mrsauravsahu/blogs/blogs.data/context/BlogsContext.cs
+++ @mrsauravsahu/blogs/blogs.data/context/BlogsContext.cs
@@ -5,6 +5,8 @@ namespace blogs.data.context
 {
     public class BlogsContext : DbContext
     {
+        public BlogsContext(DbContextOptions<BlogsContext> options) : base(options) { }
+
         public DbSet<Link> Links { get; set; }
     }
 }
