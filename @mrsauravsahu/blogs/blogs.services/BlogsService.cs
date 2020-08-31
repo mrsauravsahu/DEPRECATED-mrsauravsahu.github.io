c0cbc5b84712b35b6f5a83ea7108b106edc16dec
Saurav Sahu <mrsauravsahu@outlook.com>
2020-08-31 21:28:50 +0530

    add a BlogsService

---
+++ @mrsauravsahu/blogs/blogs.services/BlogsService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using blogs.data.context;
+using blogs.data.models;
+using blogs.services.contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace blogs.services
+{
+    public class BlogsService
+    {
+        private readonly BlogsContext blogsContext;
+
+        public BlogsService(BlogsContext blogsContext)
+        {
+            this.blogsContext = blogsContext;
+        }
+
+        public async Task<PaginatedResult<List<Blog>>> GetAllAsync()
+        {
+            var totalCount = await blogsContext.Blogs.LongCountAsync();
+            var blogs = await blogsContext.Blogs.ToListAsync();
+
+            var result = new PaginatedResult<List<Blog>>
+            {
+                Data = blogs,
+                Count = blogs.Count,
+                TotalCount = totalCount
+            };
+
+            return result;
+        }
+    }
+}
---
+++ @mrsauravsahu/blogs/blogs.services/contracts/PaginatedResult.cs
@@ -0,0 +1,9 @@
+namespace blogs.services.contracts
+{
+    public class PaginatedResult<T>
+    {
+        public T Data { get; set; }
+        public long Count { get; set; }
+        public long TotalCount { get; set; }
+    }
+}
