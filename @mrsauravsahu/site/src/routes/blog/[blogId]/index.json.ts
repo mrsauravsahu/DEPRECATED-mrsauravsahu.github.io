ef7566362ca959fe6f5860c3ab7f6e27bc761008
Saurav Sahu <mrsauravsahu@outlook.com>
2020-09-06 00:53:45 +0530

    fix API url call to get blog by id

--- @mrsauravsahu/site/src/routes/blog/[blogId]/index.json.ts
+++ @mrsauravsahu/site/src/routes/blog/[blogId]/index.json.ts
@@ -3,7 +3,7 @@ const superagent = require('superagent');
 export const get = async (req, res) => {
     const { blogId } = req.params;
 
-    const blogByIdUrl = `${process.env.BLOGS_BASE_URL}/blogs/${blogId}`;
+    const blogByIdUrl = `${process.env.BLOGS_BASE_URL}/api/blogs/${blogId}`;
     console.log(`Fetching blog with id: ${blogId} from ${blogByIdUrl}`);
     const blogByIdResponse = await superagent.get(blogByIdUrl)
     const { body: { data: blog } } = blogByIdResponse
