b8876b6177b0268f26c9792a3c3c128df4e49dc3
Saurav Sahu <mrsauravsahu@outlook.com>
2020-09-06 00:55:21 +0530

    fix route to blog post download URL

--- @mrsauravsahu/site/src/routes/blog/[blogId]/file.ts
+++ @mrsauravsahu/site/src/routes/blog/[blogId]/file.ts
@@ -3,7 +3,7 @@ const superagent = require('superagent');
 export const get = async (req, res) => {
     const { blogId, postId } = req.params;
 
-    const fileUrl = `${process.env.BLOGS_BASE_URL}/blogs/${blogId}/posts/${postId}/file`;
+    const fileUrl = `${process.env.BLOGS_BASE_URL}/api/blogs/${blogId}/file`;
     console.log(`Fetching blog content with id: ${blogId} from ${fileUrl}`);
     const fileResponse = await superagent
         .get(fileUrl)
