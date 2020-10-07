ee9c6d4f2f0e28178b5fdb0c235458b4eb31b0f7
Saurav Sahu <mrsauravsahu@outlook.com>
2020-10-07 09:58:18 +0530

    fix tests

--- @mrsauravsahu/site/cypress/integration/home/socials.spec.js
+++ @mrsauravsahu/site/cypress/integration/home/socials.spec.js
@@ -1,26 +1,31 @@
 var socials = [
-    { label: 'mrsauravsahu@outlook.com', url: 'mailto:lel' },
+    { label: 'email', url: 'mailto:mrsauravsahu@outlook.com' },
     { label: 'github', url: 'https://github.com/mrsauravsahu' },
+    { label: 'linkedIn', url: 'https://www.linkedin.com/in/mrsauravsahu' },
     { label: 'twitter', url: 'https://twitter.com/mrsauravsahu' },
-    { label: 'unsplash', url: 'https://unsplash.com/mrsauravsahu' },
     { label: 'instagram', url: 'https://instagram.com/mrsauravsahu' },
+    { label: 'unsplash', url: 'https://unsplash.com/mrsauravsahu' },
 ];
 
 describe('home page: social section', () => {
     beforeEach(() => cy.visit('/'));
 
-    // TODO: Check socials' links
+    it('all social links should be displayed', () => {
+        var socialButtons = cy.get('.social-container ss-button');
+        socialButtons.should('have.length', socials.length);
+    });
+    
     it('social links should have correct labels', () => {
         var socialButtons = cy.get('.social-container ss-button');
-        socialButtons.should('have.length', 5);
-
+        
         socialButtons.each((button, index) => {
             expect(button.get(0).innerText).equal(socials[index].label);
         });
     });
-
+    
+    // TODO: Check socials' links
     it.skip('social links should have correct links', () => {
         var socialButtons = cy.get('.social-container ss-button');
-        socialButtons.should('have.length', 5);
+        socialButtons.should('have.length', socials.length);
     });
 })
