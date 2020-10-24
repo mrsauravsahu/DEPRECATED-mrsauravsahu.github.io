1458e171a89f0de12d452fd11aff8279a79f2545
Saurav Sahu <mrsauravsahu@outlook.com>
2020-10-24 13:34:12 +0530

    separate out socials page

--- @mrsauravsahu/site/src/pages/home/socials.svelte
+++ @mrsauravsahu/site/src/pages/home/socials.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   // import { goto } from "@sapper/app";
-  import { PfButton } from "@propfull/kit";
+  import { PfCard, PfHeader } from "@propfull/kit";
   export let theme;
   export let socials = [];
 </script>
@@ -13,18 +13,24 @@
     flex-wrap: wrap;
   }
 
-  a {
+  .social-link-anchor {
     all: unset;
     width: 100%;
     cursor: pointer;
     margin: 1rem 0;
-    min-height: 8rem;
+    min-height: 6rem;
+    margin: 0.25rem;
+  }
+
+  .social-card-content{
+    display: flex;
+    justify-content: space-evenly;
   }
 
   @media only screen and (min-width: 48rem) {
-    a {
-      width: calc(50% - 1rem);
-      height: 10rem;
+    .social-link-anchor {
+      width: calc(25% - 1rem);
+      height: 6rem;
     }
   }
 </style>
@@ -33,37 +39,14 @@
 <div class="social-container ss-content">
   <p>Here's how you can get in touch with me 😊️</p>
   <div class="social-links">
-    <!-- <<<<<<< HEAD
-    <PfButton {theme} style='margin: 0.25rem' on:click={() => goto('mailto:mrsauravsahu@outlook.com')}>
-      email
-    </PfButton>
-    <PfButton {theme} style='margin: 0.25rem' on:click={() => goto('https://youtube.com/channel/UCPWETNZS6Cu3X2fYnpME32g')}>
-      youtube
-    </PfButton>
-    <PfButton {theme} style='margin: 0.25rem' on:click={() => goto('https://github.com/mrsauravsahu')}>
-      github
-    </PfButton>
-    <PfButton {theme} style='margin: 0.25rem'
-      on:click={() => goto('https://www.linkedin.com/in/mrsauravsahu/')}>
-      linkedIn
-    </PfButton>
-    <PfButton {theme} style='margin: 0.25rem' on:click={() => goto('https://twitter.com/mrsauravsahu')}>
-      twitter
-    </PfButton>
-    <PfButton {theme} style='margin: 0.25rem' on:click={() => goto('https://instagram.com/mrsauravsahu')}>
-      instagram
-    </PfButton>
-    <PfButton {theme} style='margin: 0.25rem' on:click={() => goto('https://unsplash.com/@mrsauravsahu')}>
-      unsplash
-    </PfButton>
-======= -->
     {#each socials as socialLink}
-      <a href={socialLink.url}>
-        <PfButton {theme} style="margin: 0.25rem">
-          {socialLink.label}
-        </PfButton>
+      <a class="social-link-anchor" href={socialLink.url}>
+        <PfCard {theme}>
+          <div slot="content" class="social-card-content">
+            <PfHeader theme='dark' type='h2'>{socialLink.label}</PfHeader>
+          </div>
+        </PfCard>
       </a>
     {/each}
-    <!-- >>>>>>> 03def4c... retrieve socials data from local data -->
   </div>
 </div>
--- @mrsauravsahu/site/src/routes/index.svelte
+++ @mrsauravsahu/site/src/routes/index.svelte
@@ -2,23 +2,16 @@
   export async function preload({ params }) {
     const response = await this.fetch("home.json");
     const { highlights, latestBlog } = await response.json();
-
-    // Get socials data
-    const socialsUrl = "/data/socials.json";
-    console.log(`Fetching socials data from: ${socialsUrl}`);
-    const socialsResponse = await this.fetch(socialsUrl);
-    const { data: socials } = await socialsResponse.json();
-    return { highlights, latestBlog, socials };
+    return { highlights, latestBlog };
   }
 </script>
 
 <script lang="ts">
-  export let highlights, latestBlog, socials;
-  
+  export let highlights, latestBlog;
+
   import { theme } from "../stores/theme";
   import About from "../pages/home/about.svelte";
   import Highlights from "../pages/home/highlights.svelte";
-  import Socials from "../pages/home/socials.svelte";
   import Tag from "../pages/home/tag.svelte";
   import WhatsNew from "../pages/home/whats-new.svelte";
 </script>
@@ -31,4 +24,3 @@
 <About />
 <WhatsNew {latestBlog} />
 <Highlights projects={highlights} theme={$theme} />
-<!-- <Socials theme={$theme} {socials} /> -->
