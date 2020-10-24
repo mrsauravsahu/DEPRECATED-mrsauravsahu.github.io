78f5856b10eeb9052f82226493f058b238fa3930
Saurav Sahu <mrsauravsahu@outlook.com>
2020-10-24 15:56:34 +0530

    update socials to use font awesome icons

--- @mrsauravsahu/site/src/pages/home/socials.svelte
+++ @mrsauravsahu/site/src/pages/home/socials.svelte
@@ -1,11 +1,33 @@
 <script lang="ts">
-  // import { goto } from "@sapper/app";
+  import Icon from "svelte-awesome/components/Icon.svelte";
+  import {
+    faYoutube,
+    faTwitter,
+    faGithub,
+    faLinkedin,
+    faInstagram,
+    faUnsplash,
+  } from "@fortawesome/free-brands-svg-icons";
+  import { faEnvelopeOpen } from "@fortawesome/free-regular-svg-icons";
+
   import { PfCard, PfHeader } from "@propfull/kit";
   export let theme;
-  export let socials = [];
 </script>
 
 <style>
+
+  section {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  .main-content {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+  }
+
   .social-links {
     display: flex;
     flex-direction: row;
@@ -18,35 +40,92 @@
     width: 100%;
     cursor: pointer;
     margin: 1rem 0;
-    min-height: 6rem;
+    min-height: 8rem;
     margin: 0.25rem;
   }
 
-  .social-card-content{
+  .social-card-content {
     display: flex;
     justify-content: space-evenly;
+    align-items: center;
   }
 
   @media only screen and (min-width: 48rem) {
     .social-link-anchor {
       width: calc(25% - 1rem);
-      height: 6rem;
+      height: 8rem;
     }
   }
 </style>
 
-<ss-title id="socials">--socials</ss-title>
-<div class="social-container ss-content">
-  <p>Here's how you can get in touch with me 😊️</p>
-  <div class="social-links">
-    {#each socials as socialLink}
-      <a class="social-link-anchor" href={socialLink.url}>
-        <PfCard {theme}>
-          <div slot="content" class="social-card-content">
-            <PfHeader theme='dark' type='h2'>{socialLink.label}</PfHeader>
-          </div>
-        </PfCard>
-      </a>
-    {/each}
+<section>
+  <ss-title id="socials">--socials</ss-title>
+  <div class="main-content">
+    <div class="social-container ss-content">
+      <p>Here's how you can get in touch with me. Links to all socials.</p>
+
+      <div class="social-links">
+        <a class="social-link-anchor" href="mailto:mrsauravsahu@outlook.com">
+          <PfCard {theme}>
+            <div slot="content" class="social-card-content">
+              <PfHeader theme="dark" type="h2">email</PfHeader>
+              <Icon data={faEnvelopeOpen} scale={2.5} />
+            </div>
+          </PfCard>
+        </a>
+        <a
+          class="social-link-anchor"
+          href="https://youtube.com/channel/UCPWETNZS6Cu3X2fYnpME32g">
+          <PfCard {theme}>
+            <div slot="content" class="social-card-content">
+              <PfHeader theme="dark" type="h2">youtube</PfHeader>
+              <Icon data={faYoutube} scale={2.5} />
+            </div>
+          </PfCard>
+        </a>
+        <a class="social-link-anchor" href="https://twitter.com/mrsauravsahu">
+          <PfCard {theme}>
+            <div slot="content" class="social-card-content">
+              <PfHeader theme="dark" type="h2">twitter</PfHeader>
+              <Icon data={faTwitter} scale={2.5} />
+            </div>
+          </PfCard>
+        </a>
+        <a class="social-link-anchor" href="https://github.com/mrsauravsahu">
+          <PfCard {theme}>
+            <div slot="content" class="social-card-content">
+              <PfHeader theme="dark" type="h2">github</PfHeader>
+              <Icon data={faGithub} scale={2.5} />
+            </div>
+          </PfCard>
+        </a>
+        <a
+          class="social-link-anchor"
+          href="https://www.linkedin.com/in/mrsauravsahu/">
+          <PfCard {theme}>
+            <div slot="content" class="social-card-content">
+              <PfHeader theme="dark" type="h2">linkedIn</PfHeader>
+              <Icon data={faLinkedin} scale={2.5} />
+            </div>
+          </PfCard>
+        </a>
+        <a class="social-link-anchor" href="https://instagram.com/mrsauravsahu">
+          <PfCard {theme}>
+            <div slot="content" class="social-card-content">
+              <PfHeader theme="dark" type="h2">instagram</PfHeader>
+              <Icon data={faInstagram} scale={2.5} />
+            </div>
+          </PfCard>
+        </a>
+        <a class="social-link-anchor" href="https://unsplash.com/@mrsauravsahu">
+          <PfCard {theme}>
+            <div slot="content" class="social-card-content">
+              <PfHeader theme="dark" type="h2">unsplash</PfHeader>
+              <Icon data={faUnsplash} scale={2.5} />
+            </div>
+          </PfCard>
+        </a>
+      </div>
+    </div>
   </div>
-</div>
+</section>
