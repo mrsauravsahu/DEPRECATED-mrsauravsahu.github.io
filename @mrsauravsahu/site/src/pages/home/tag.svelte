eb3cd2d6b60107b0d299dea053b57b80ee8666fa
Saurav Sahu <mrsauravsahu@outlook.com>
2020-10-24 15:58:07 +0530

    fix: top margin for tag component

--- @mrsauravsahu/site/src/pages/home/tag.svelte
+++ @mrsauravsahu/site/src/pages/home/tag.svelte
@@ -1,18 +1,19 @@
 <style>
-    .about-line-container {
-        text-align: center;
-    }
+  .about-line-container {
+    margin-top: 2rem;
+    text-align: center;
+  }
 
-    .about-line-container > em {
-        font-weight: 400;
-    }
+  em {
+    font-weight: 400;
+  }
 </style>
 
 <div class="content about-line-container">
-    <em>
-        My journey and experiences will always remain a work in progress... like
-        this website will never be complete. 😂
-        <br />
-        Je parle un peu le français et apprends tous les jours.
-    </em>
+  <em>
+    My journey and experiences will always remain a work in progress... like
+    this website will never be complete. 😂
+    <br />
+    Je parle un peu le français et apprends tous les jours.
+  </em>
 </div>
