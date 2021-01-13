b33fbf539cf016edf76497af83939c2efde4f366
Saurav Sahu <mrsauravsahu@outlook.com>
2021-01-13 17:43:06 +0530

    fix nullability for Links Model

--- blogs/blogs.data/models/Link.cs
+++ blogs/blogs.data/models/Link.cs
@@ -5,9 +5,9 @@ namespace blogs.data.models
     public class Link
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Url { get; set; }
+        public string Title { get; set; } = default!;
+        public string? Description { get; set; }
+        public string Url { get; set; } = default!;
         public DateTime CreatedAt { get; set; }
     }
 }
