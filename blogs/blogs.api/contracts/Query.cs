c1b29fe53ff003dac06e44c3279e8dcff8928e55
Saurav Sahu <mrsauravsahu@outlook.com>
2021-01-13 17:11:18 +0530

    add get blog by id

--- blogs/blogs.api/contracts/Query.cs
+++ blogs/blogs.api/contracts/Query.cs
@@ -1,6 +1,8 @@
 using System.Linq;
+using System.Threading.Tasks;
 using blogs.data.context;
 using blogs.data.models;
+using blogs.services;
 using HotChocolate;
 
 namespace blogs.api.contracts
@@ -9,5 +11,7 @@ namespace blogs.api.contracts
     {
         public IQueryable<Blog> GetBlogs([Service] BlogsContext dbContext) => dbContext.Blogs;
         public IQueryable<Link> GetLinks([Service] BlogsContext dbContext) => dbContext.Links;
+        public Task<Blog> GetBlogByIdAsync(BlogRouteParams input, [Service] BlogsService blogsService)
+        => blogsService.GetByIdAsync(input.Id);
     }
 }
