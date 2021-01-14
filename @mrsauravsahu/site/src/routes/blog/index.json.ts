b514bf3abcf6568650ebcebcb1ace2851c24a973
Saurav Sahu <mrsauravsahu@outlook.com>
2021-01-15 00:23:09 +0530

    use graphQL in blog listing page

--- @mrsauravsahu/site/src/routes/blog/index.json.ts
+++ @mrsauravsahu/site/src/routes/blog/index.json.ts
@@ -1,13 +1,31 @@
+import { urqlClient } from "../../setup/urql";
+
 const superagent = require('superagent');
 
 export const get = async (_, res) => {
     const dataUrl = `${process.env.BLOGS_BASE_URL}/api/blogs?sorts=-createdAt`;
     console.log(`Fetching blogs from: ${dataUrl}`)
-    const allBlogsResponse = await superagent.get(dataUrl)
-    const json = allBlogsResponse.body
-    const { data: blogs, count, totalCount } = json;
 
-    const jsonString = JSON.stringify({ blogs, count, totalCount });
+    const allBlogsResponse = await urqlClient.query(`
+    {
+        blogs(order: {
+            createdAt: DESC
+        }) {
+            id
+            title
+            description
+            createdAt
+            approxTimeToRead
+        }
+
+        totalCount: blogsCount
+    }
+    `).toPromise()
+
+    const jsonString = JSON.stringify({
+       ...allBlogsResponse.data,
+       count: 1
+    });
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(jsonString);
 }
