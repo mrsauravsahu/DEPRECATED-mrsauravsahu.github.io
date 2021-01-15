5a5a9acae59c953379d5a91ebc19c3f1a9d78f56
Saurav Sahu <mrsauravsahu@outlook.com>
2021-01-15 16:54:16 +0530

    use graphQL in blog page

--- @mrsauravsahu/site/src/routes/blog/index.json.ts
+++ @mrsauravsahu/site/src/routes/blog/index.json.ts
@@ -1,30 +1,50 @@
 import { urqlClient } from "../../setup/urql";
 
-const superagent = require('superagent');
-
 export const get = async (_, res) => {
     const dataUrl = `${process.env.BLOGS_BASE_URL}/api/blogs?sorts=-createdAt`;
     console.log(`Fetching blogs from: ${dataUrl}`)
 
-    const allBlogsResponse = await urqlClient.query(`
-    {
-        blogs(order: {
-            createdAt: DESC
-        }) {
-            id
-            title
-            description
-            createdAt
-            approxTimeToRead
+    let state = {
+        endCursor: undefined,
+        hasNextPage: true
+    }
+
+    let blogs = [];
+
+    do {
+        const allBlogsResponse = await urqlClient.query(`
+        query ($after: String) {
+            blogs(first: 1, after: $after, order: {
+                createdAt: DESC
+            }) {
+                nodes {
+                    id
+                    title
+                    description
+                    createdAt
+                    approxTimeToRead
+                }
+
+                pageInfo {
+                    endCursor
+                    startCursor
+                    hasNextPage
+                }
+            }
+        }
+    `, { after: state.endCursor }).toPromise()
+
+        blogs = [...blogs, ...allBlogsResponse.data.blogs.nodes]
+        state = {
+            hasNextPage: allBlogsResponse.data.blogs.pageInfo.hasNextPage,
+            endCursor: allBlogsResponse.data.blogs.pageInfo.endCursor
         }
 
-        totalCount: blogsCount
-    }
-    `).toPromise()
+    } while (state.hasNextPage);
 
     const jsonString = JSON.stringify({
-       ...allBlogsResponse.data,
-       count: 1
+        blogs,
+        totalCount: blogs.length
     });
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(jsonString);
