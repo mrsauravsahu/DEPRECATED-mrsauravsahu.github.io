e3c0ec8acc7d44589fbc489e9730faa3ba2936f0
Saurav Sahu <mrsauravsahu@outlook.com>
2021-01-15 14:33:40 +0530

    add get highlights query

--- blogs/blogs.api/contracts/Query.cs
+++ blogs/blogs.api/contracts/Query.cs
@@ -1,12 +1,18 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+// using blogs.api.extensions;
+// using blogs.api.gql;
 using blogs.data.context;
 using blogs.data.models;
 using blogs.services;
+using blogs.services.contracts;
 using HotChocolate;
 using HotChocolate.Data;
 using HotChocolate.Types;
+using HotChocolate.Types.Pagination;
 using Microsoft.EntityFrameworkCore;
+using Sieve.Models;
 
 namespace blogs.api.contracts
 {
@@ -15,10 +21,18 @@ namespace blogs.api.contracts
         [UsePaging]
         [UseSorting]
         public IQueryable<Blog> GetBlogs([Service] BlogsContext dbContext) => dbContext.Blogs.OrderByDescending(p => p.CreatedAt);
+
         public IQueryable<Link> GetLinks([Service] BlogsContext dbContext) => dbContext.Links;
+        
         public Task<Blog> GetBlogByIdAsync(BlogRouteParams input, [Service] BlogsService blogsService)
         => blogsService.GetByIdAsync(input.Id);
 
         public Task<int> BlogsCount([Service] BlogsContext dbContext) => dbContext.Blogs.CountAsync();        
+
+        public async Task<IQueryable<HighlightDto>> GetHighlightsAsync([Service] HighlightsService highlightsService)
+        {
+            var highlights = await highlightsService.GetAllAsync(new SieveModel());
+            return highlights.Data.AsQueryable();
+        }
     }
 }
