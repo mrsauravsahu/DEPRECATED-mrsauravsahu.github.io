daf5e4306df1a31cb14dd18c5be22d5861057518
Saurav Sahu <mrsauravsahu@outlook.com>
2021-01-15 14:28:59 +0530

    add paging to blogs

--- blogs/blogs.api/contracts/Query.cs
+++ blogs/blogs.api/contracts/Query.cs
@@ -5,18 +5,20 @@ using blogs.data.models;
 using blogs.services;
 using HotChocolate;
 using HotChocolate.Data;
+using HotChocolate.Types;
 using Microsoft.EntityFrameworkCore;
 
 namespace blogs.api.contracts
 {
     public class Query
     {
+        [UsePaging]
         [UseSorting]
-        public IQueryable<Blog> GetBlogs([Service] BlogsContext dbContext) => dbContext.Blogs;
+        public IQueryable<Blog> GetBlogs([Service] BlogsContext dbContext) => dbContext.Blogs.OrderByDescending(p => p.CreatedAt);
         public IQueryable<Link> GetLinks([Service] BlogsContext dbContext) => dbContext.Links;
         public Task<Blog> GetBlogByIdAsync(BlogRouteParams input, [Service] BlogsService blogsService)
         => blogsService.GetByIdAsync(input.Id);
 
-        public Task<int> BlogsCount([Service] BlogsContext dbContext) => dbContext.Blogs.CountAsync();
+        public Task<int> BlogsCount([Service] BlogsContext dbContext) => dbContext.Blogs.CountAsync();        
     }
 }
