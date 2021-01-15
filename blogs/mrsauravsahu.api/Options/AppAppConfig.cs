a9a72edb59e0956a3526d9e417293b0635dc8bcd
Saurav Sahu <mrsauravsahu@outlook.com>
2021-01-15 16:30:02 +0530

    fixed nullable issues

--- blogs/mrsauravsahu.api/Options/AppAppConfig.cs
+++ blogs/mrsauravsahu.api/Options/AppAppConfig.cs
@@ -2,14 +2,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace mrsauravsahu.api.options
 {
-    public class AboutAppOptions {
+    public class AboutAppOptions
+    {
         [Required]
-        public string AppName { get; set; }
+        public string AppName { get; set; } = default!;
 
         [Required]
-        public string Version { get; set; }
+        public string Version { get; set; } = default!;
 
         [Required, EmailAddress]
-        public string ContactEmail { get; set; }
+        public string ContactEmail { get; set; } = default!;
     }
 }
