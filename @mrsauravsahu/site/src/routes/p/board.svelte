233aa04ab1e6fee1d883ab1e085483c02f7d4347
Saurav Sahu <mrsauravsahu@outlook.com>
2022-02-08 20:19:46 +0530

    Update board.svelte (#162)

--- @mrsauravsahu/site/src/routes/p/board.svelte
+++ @mrsauravsahu/site/src/routes/p/board.svelte
@@ -1,5 +1,5 @@
 <script lang='ts' context='module'>
   export function preload() {
-    this.redirect(302, 'https://foregoing-taker-7cd.notion.site/e96d8cd6ff6e452ca4b58d16940793b5')
+    this.redirect(302, 'https://mrsauravsahu.notion.site/e96d8cd6ff6e452ca4b58d16940793b5')
   }
 </script>
